d1611b13c36f291f173a83e0580bbe89c1c83b17
Jonsson <kzmq426@astrazeneca.net>
2022-05-09 15:18:38 +0200

    thesis code

--- run_notebook.sh
+++ run_notebook.sh
@@ -1,6 +1,4 @@
 
 docker build -t notebook .
 
-#docker run -p 8888:8888 --mount /Users/kzmq426/'OneDrive - AZCollaboration'/Desktop/Master-Thesis:/home/jovyan/work notebook
-
-docker run -it -p 8888:8888 -v //c/Users/kzmq426/'OneDrive - AZCollaboration'/Desktop/Master-Thesis:/home/jovyan/work --rm --name jupyter notebook
+docker run -it -p 8888:8888 -v /$(pwd):/home/jovyan/work --rm --name jupyter notebook
